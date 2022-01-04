e46207bd0d07a7e055ac3cac89755008c6b87abe
h-miiaah <haim.miiaah5@gmail.com>
2022-01-03 21:34:54 -0500

    comments

--- LengthOfLastWord.js
+++ LengthOfLastWord.js
@@ -6,3 +6,14 @@
     const words = s.trim().split(" ");
     return words.length > 0 ? words[words.length - 1].length : 0;
 };
+
+
+/*
+
+- we need to trim the given string to remove the extra spaces
+- split the string into separate words using the space character
+- check to see if there are words whos length is greater than 0
+- return the length of the last word
+- otherwise we return zero
+
+*/
